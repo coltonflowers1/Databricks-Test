bdd278c3e00a54e61eeedcf69acc4aef2ad6e093
coltonflowers1 <coltonflowers@email.arizona.edu>
2022-04-29 18:59:05 -0400

    .

--- monorepo/package_2/setup.py
+++ monorepo/package_2/setup.py
@@ -4,6 +4,6 @@ setup(
   name='package_2',
   version='0.1.0',
   python_requires='>=3.8',
-  modules=["some_more_code"],
+  packages=["package_2"],
   install_requires=[f"package_1 @ file://localhost/{Path(__file__).parents[1].resolve()}/Databricks-Test/monorepo/package_1#egg=package_1"]
 )
