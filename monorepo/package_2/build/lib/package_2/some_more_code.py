f7296a24d8601b974e19fc8da23b4797e8355bf7
coltonflowers1 <coltonflowers@email.arizona.edu>
2022-05-02 10:29:04 -0400

    created local_pkg function

---
+++ monorepo/package_2/build/lib/package_2/some_more_code.py
@@ -0,0 +1,3 @@
+import package_1.some_code as some_code
+
+print(some_code.FRUIT_VAR)
--- monorepo/package_2/setup.py
+++ monorepo/package_2/setup.py
@@ -2,12 +2,20 @@ import os
 from pathlib import Path
 import sys
 from setuptools import setup
+
 IN_DATABRICKS = "DATABRICKS_RUNTIME_VERSION" in os.environ
+def local_pkg(name: str) -> str:
+    """Returns a path to a local package."""
+    if IN_DATABRICKS: 
+      return f'{name} @ file://localhost/databricks/driver/Databricks-Test/monorepo/name'
+    else:
+      return f'{name} @ file://localhost/{Path.cwd().parent / name}'
+
 local_install_requirement_location =  "/databricks/driver/Databricks-Test/monorepo" if IN_DATABRICKS else Path.cwd().parent
 setup(
   name='package_2',
   version='0.1.0',
   python_requires='>=3.8',
   packages=["package_2"],
-  install_requires=[f"package_1 @ file://localhost/{local_install_requirement_location}/package_1#egg=package_1"] 
+  install_requires=[local_pkg("package_1")] 
 )
