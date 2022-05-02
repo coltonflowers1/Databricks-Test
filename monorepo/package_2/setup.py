6db2ec10d101f807069311b7b199f2c0e249c378
coltonflowers1 <coltonflowers@email.arizona.edu>
2022-05-02 14:18:06 -0400

    added databricks secrets

--- monorepo/package_2/setup.py
+++ monorepo/package_2/setup.py
@@ -1,14 +1,17 @@
 import os
 from pathlib import Path
-import sys
 from setuptools import setup
-
+  
 IN_DATABRICKS = "DATABRICKS_RUNTIME_VERSION" in os.environ
+if IN_DATABRICKS: 
+  github_api_key = dbutils.secrets.get(scope="github", key="coltons_api_key")
+
 def local_pkg(name: str) -> str:
     """Returns a path to a local package."""
-    if IN_DATABRICKS: 
-      return f'{name} @ file://localhost/databricks/driver/Databricks-Test/monorepo/name'
-    else:
+    if IN_DATABRICKS: #pull from github repo, will need to specify api key here.
+      # TODO: Retrive Github API key using secret. 
+      return f'{name} @ git+https://{github_api_key}@github.com/coltonflowers1/Databricks-Test@master#subdirectory=monorepo/{name}'
+    else: #pull from local files
       return f'{name} @ file://localhost/{Path.cwd().parent / name}'
 
 local_install_requirement_location =  "/databricks/driver/Databricks-Test/monorepo" if IN_DATABRICKS else Path.cwd().parent
@@ -17,5 +20,5 @@ setup(
   version='0.1.0',
   python_requires='>=3.8',
   packages=["package_2"],
-  install_requires=[local_pkg("package_1")] 
+  install_requires=[local_pkg("package_1")]
 )
