8737d10a1f3fad677741959318288374a6563239
coltonflowers1 <coltonflowers@email.arizona.edu>
2022-05-02 09:18:05 -0400

    fixed url

--- monorepo/package_2/setup.py
+++ monorepo/package_2/setup.py
@@ -2,12 +2,13 @@ import os
 from pathlib import Path
 import sys
 from setuptools import setup
-# IN_DATABRICKS = sys.env.contains("DATABRICKS_RUNTIME_VERSION")
-local_install_requirement_location =  os.getcwd() 
+IN_DATABRICKS = sys.env.contains("DATABRICKS_RUNTIME_VERSION")
+# local_install_requirement_location =  os.getcwd() if IN_DATABRICKS else "/databricks/driver/"
+local_install_requirement_location =  os.getcwd() #if IN_DATABRICKS else "/databricks/driver/"
 setup(
   name='package_2',
   version='0.1.0',
   python_requires='>=3.8',
   packages=["package_2"],
-  install_requires=[f"package_1 @ file://localhost/driver/{local_install_requirement_location}/monorepo/package_1#egg=package_1"] 
+  install_requires=[f"package_1 @ file://localhost/{local_install_requirement_location}/monorepo/package_1#egg=package_1"] 
 )
